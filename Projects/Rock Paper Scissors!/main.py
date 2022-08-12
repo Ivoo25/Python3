f53ff8dc3036ba9d4de5d177c45d431c0695ab8e
Ivo <agustinferrari15@gmail.com>
2022-08-12 11:42:27 -0300

    Added random choices to computer

--- Projects/Rock Paper Scissors!/main.py
+++ Projects/Rock Paper Scissors!/main.py
@@ -1,18 +1,32 @@
+import random as rand
+
 def get_choices():
+    option = ["rock", "paper", "scissors"]
     player_choice = input("Rock, Paper, or Scissors?: ")
-    if(player_choice.lower() not in ["rock", "paper", "scissors"]):
+    if(player_choice.lower() not in option):
         print("Invalid choice!")
         return get_choices()
     
-    computer_choice = "paper"
+    computer_choice = rand.choice(option)
     choices = {
         "player" : player_choice,
         "computer" : computer_choice
     }
     return choices
 
+def get_winner(choices):
+    if(choices["player"] == choices["computer"]):
+        return "tie"
+    elif(choices["player"] == "rock" and choices["computer"] == "scissors"):
+        return "player"
+    elif(choices["player"] == "paper" and choices["computer"] == "rock"):
+        return "player"
+    elif(choices["player"] == "scissors" and choices["computer"] == "paper"):
+        return "player"
+    else:
+        return "computer"
 
 
+winner = get_winner(get_choices())
 
-choices = get_choices()
-print(choices)
+print("And the winner is... {}!".format(winner))
