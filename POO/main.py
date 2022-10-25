133737b510d348e5b28154bd6ff85c6802d81b31
Ivo Agustín Ferrari <iferrari@practia.global>
2022-10-25 10:56:55 -0300

    POO

--- POO/main.py
+++ POO/main.py
@@ -2,7 +2,7 @@ class Pajaro():
     
     __private_alas = True
     
-    def __init__(self, color = "sin color", especie = "sin especie"):
+    def __init__(self, color = "sin color", especie = "comun"):
         self.__private_color = color # Atributo privado, solo se puede acceder desde la clase
         self.__private_especie = especie # Atributo privado, solo se puede acceder desde la clase
         
@@ -20,6 +20,12 @@ class Pajaro():
     
     def get_alas(self):
         return self.__private_alas
+    
+    def piar(self):
+        print("pio pio, mi color es {} y soy un {}".format(self.__private_color, self.__private_especie))
+        
+    def volar(self, metros):
+        print("Vole {} metros".format(metros))	    
 
 
 mi_pajaro = Pajaro()
@@ -28,3 +34,5 @@ print(mi_pajaro.get_color())
 mi_pajaro.set_color("verde")
 print(mi_pajaro.get_color())
 print(mi_pajaro.get_alas())
+mi_pajaro.volar(20)
+mi_pajaro.piar()
